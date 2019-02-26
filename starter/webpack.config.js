5de9110e82bab971249cc224fa0a805424ac37ad
Jacques Raguin <jak.rag@gmail.com>
2019-02-26 20:19:27 +0100

    add jest lib

--- starter/webpack.config.js
+++ starter/webpack.config.js
@@ -27,6 +27,21 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },

376376309fe798df1613dd092e798b5281cc117b
JacquesRogueOne <drdoomjax@hotmail.com>
2019-03-09 18:12:46 +0100

    fix css loader for real

--- starter/src/js/components/main/components/add-player/components/add-player-field/index.jsx
+++ starter/src/js/components/main/components/add-player/components/add-player-field/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import style from './style.css';
 
 const AddPlayerField = () => {
 
   return (
-    <div className="addPlayerField">
+    <div className={style.addPlayerField}>
         <label id="" for="addPlayerInput">Ajouter un joueur</label>
         <input id="addPlayerInput" type="text" />
     </div>

c5ab576d445e49cb368a3eec0cba2012ca7d7816
Jacques-Raguin <jacques.raguin.externe@darty.fr>
2019-05-22 13:19:02 +0200

    do Ismail fix to try

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
@@ -12,3 +12,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port);
+
+console.log('Server started on port ' + port);
